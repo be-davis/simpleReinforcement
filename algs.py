eb0ab5530507b3d407178f15f394b70340e7f6c9
Brandon Davis <brandon.davis9@gmail.com>
2022-03-09 15:15:57 -0500

    remove readme

--- algs.py
+++ algs.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from scipy.special import softmax
     
 class QLearning: 
-    def __init__(self, alpha=0.9, gamma=0.8, epsilon=0.5, policy='softmax', env=FourRooms(7)):
+    def __init__(self, alpha=0.9, gamma=0.8, epsilon=0.5, policy='eps_greedy', env=HairpinMaze(10)):
         #starting aparameters
         self.alpha = alpha
         self.gamma = gamma
@@ -15,17 +15,23 @@ class QLearning:
         #to indicate what maze you may want to run the algorithm on
         self.env = env 
         self.q_table = np.zeros([self.env.num_spaces, self.env.num_actions])
-    def sample_action(self, p):
+        self.policy = policy
+    
+    def sample_action(self, p, state):
         if p == 'softmax':
             action = npr.choice(self.env.num_actions, p=softmax(1e6 * self.q_table[self.env.coord_to_discrete(state)]))
-        if p == 'eps_greedy':
+        elif p == 'eps_greedy':
             if npr.uniform(0,1)<self.epsilon:
-                    action = random.randint(0,self.env.num_actions-1)
+                action = random.randint(0,self.env.num_actions-1)
                 
             # rand > eps means take action from state with highest q value
             else:
-                action = np.argmax(self.q_table[self.env.coord_to_discrete(state)])
-    def run_alg(self, episodes=30):
+                #action = np.argmax(self.q_table[self.env.coord_to_discrete(state)])
+                action = npr.choice(np.flatnonzero(np.isclose([self.env.coord_to_discrete(state), max(self.q_table[self.env.coord_to_discrete(state)]))))
+        else:
+            raise Exception("Not a valid policy")
+        return action
+    def run_alg(self, episodes=300):
         
 
         #list to hold the number of steps for each episode
@@ -33,37 +39,23 @@ class QLearning:
 
         for episode in range(1, episodes+1):
             
-            #initial state of each episode is the start state
-                #as defined by the environment
+            
             state = self.env.start_state
             self.env.agent_state = self.env.start_state
-            #state = 0
-            #print(state)
+            
             steps, reward, score = 0,0,0
 
             done = False
 
             while not done:
-                #epsilon greedy--> rand < eps means explore
+                #print([state])
+                action = self.sample_action(self.policy, state)
                 
-                #sampling action from softmax distribution
-                action = npr.choice(self.env.num_actions, p=softmax(1e6 * self.q_table[self.env.coord_to_discrete(state)]))
-                action = self.sample_action(policy)
-                #if npr.uniform(0,1)<self.epsilon:
-                #    action = random.randint(0,self.env.num_actions-1)
-                
-                # rand > eps means take action from state with highest q value
-                #else:
-                #    action = np.argmax(self.q_table[self.env.coord_to_discrete(state)])
-
-
                 next_state, reward, done = self.env.step(action)
-                #print([state, next_state, reward,done])
+                
                 #this old q value will be replaced by new_q
                 old_q = self.q_table[self.env.coord_to_discrete(state), action]
 
-                #print(next_state)
-                #print(self.q_table.shape)
                 #q value at best action to take at the next state
                 best_next = np.max(self.q_table[self.env.coord_to_discrete(next_state)])
 
@@ -79,5 +71,37 @@ class QLearning:
         print(min(all_steps))
         #print(min(all_steps))
         #return all_steps
-    
-    
+"""    
+class ValIteration:
+    def __init__(self, env=FourRooms(7), gamma):
+        self.env = env
+        self.gamma = gamma
+        self.t = np.zeros((self.env.num_spaces, self.env.num_actions, self.env.num_spaces))
+
+        #initializing transition matrix
+        self.to_left = lambda s: s-1
+        self.to_right = lambda s: s+1
+        self.below = lambda s: s+self.num_cols
+        self.above = lambda s: s-self.num_cols
+        for s in range(len(self.t)):
+            for a in range(len(self.t[0])):
+                if s not in self.wall_cells:
+                    self.t[self.env.coord_to_discrete(s)][0][self.above(s)] = 1
+                    self.t[self.env.coord_to_discrete(s)][1][self.below(s)] = 1
+                    self.t[self.env.coord_to_discrete(s)][2][self.to_right(s)] = 1
+                    self.t[self.env.coord_to_discrete(s)][3][self.to_left(s)] = 1
+                    if self.above(s) in self.wall_cells:
+                        self.t[self.env.coord_to_discrete(s)][0][self.above(s)] = 0
+                    if self.below(s) in self.wall_cells:
+                        self.t[self.env.coord_to_discrete(s)][1][self.below(s)] = 0
+                    if self.to_right(s) in self.wall_cells:
+                        self.t[self.env.coord_to_discrete(s)][2][self.to_right(s)] = 0
+                    if self.to_left(s) in self.wall_cells:
+                        self.t[self.env.coord_to_discrete(s)][3][self.to_left(s)] = 0  
+    def bellman(self, V):
+        for s in range(self.env.num_spaces):
+            action_vals = []
+            for a in range(self.env.num_actions):
+                val_1 = 0
+                val_2 = self.env.rewards
+"""
--- simple_grid_envs.py
+++ simple_grid_envs.py
@@ -138,3 +138,81 @@ class FourRooms:
         plt.axis('off')
         plt.imshow(self.grid)
 
+class HairpinMaze:
+    def __init__(self, wall_size, reward=10):
+        self.wall_size = wall_size
+        self.num_rows = self.wall_size + 2 #+2 because of end walls
+        self.num_cols = 11
+        self.num_spaces = self.num_rows * self.num_cols
+        self.num_actions = 4
+        self.reward = reward
+        self.start_state = [self.num_cols-1, 1] #bottom left side
+        self.agent_state = self.start_state
+        self.goal_state = [1, self.num_cols -2] # top right side
+
+        self.wall_col_positions = [2,4,6,8]
+        self.bottom_wall_cols = [2,6]
+        self.top_wall_cols = [4,8]
+        self.bottom_wall_rows = [i for i in range(self.num_cols, 1, -1)]
+        self.top_wall_rows = [i for i in range(1, self.wall_size)]
+        self.wall_cells = []
+        
+        #adding walls for hallways
+        for row in self.bottom_wall_rows:
+            for col in self.bottom_wall_cols:
+                self.wall_cells.append([row,col])
+        for row in self.top_wall_rows:
+            for col in self.top_wall_cols:
+                self.wall_cells.append([row,col])
+        self.wall_cells = np.array(self.wall_cells)
+        #add border walls
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                if i ==0 or j == 0:
+                    self.wall_cells = np.append(self.wall_cells, np.array([[i,j]]), axis=0)
+                if i == self.num_rows-1 or j == self.num_cols -1:
+                    self.wall_cells = np.append(self.wall_cells, np.array([[i,j]]), axis=0)
+    def coord_to_discrete(self, state):
+        """takes the state corrdinate [row,col] and turns it into a sate (ex. 1,2,3..)"""
+        discrete_vals = []
+        for i in range(self.num_rows):
+            discrete_vals.append(np.arange(self.num_cols*i, self.num_cols*(i+1)))
+        return discrete_vals[state[0]][state[1]]
+    def step(self, action):
+        if action == 0:
+            self.next_state = [self.agent_state[0]-1, self.agent_state[1]]
+        elif action == 1:
+            self.next_state = [self.agent_state[0]+1, self.agent_state[1]]
+        elif action == 2:
+            self.next_state = [self.agent_state[0], self.agent_state[1]+1]
+        elif action == 3:
+            self.next_state = [self.agent_state[0], self.agent_state[1]-1]
+        else:
+            raise Exception("Not a valid action!")
+        
+        
+                
+        if self.next_state in self.wall_cells.tolist():
+            #print('hi')
+            self.agent_state = self.agent_state
+        else:
+            self.agent_state = self.next_state
+        
+        if self.agent_state == self.goal_state:
+            done = True
+            reward = self.reward
+        else:
+            done = False
+            reward = 0 
+        
+        return self.agent_state, reward, done
+    def visualize(self):
+        self.grid = np.zeros((self.num_rows, self.num_cols))
+        #print(self.grid)
+        self.grid[self.agent_state[0]][self.agent_state[1]] = 1
+        self.grid[self.goal_state[0]][self.goal_state[1]] = 3
+        for wall in self.wall_cells:
+        #    print(wall)
+            self.grid[wall[0]][wall[1]] = 2
+        plt.axis('off')
+        plt.imshow(self.grid)
